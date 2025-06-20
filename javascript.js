8d1c8036ea948f0ba622569fa8210edfeaefcd82
Reyuken <212830200+Reyuken@users.noreply.github.com>
2025-06-20 21:00:25 +0800

    Created the following functions&variables -getComputerChoice: provides randomly generated choice -getHumanChoice: asks for user input -playRound: code of the game (to edit) -humanScore: stores user score -computeScore: stores computer score

--- javascript.js
+++ javascript.js
@@ -1,1 +1,33 @@
-console.log("hellooooo")
+let humanScore=0;
+let computeScore=0;
+
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random()*99);
+      if (randomNumber<=33){
+        return "Rock";
+
+        }
+    else if(randomNumber>66){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+    
+    }
+   
+function getHumanChoice(user_choice){
+    user_choice=prompt("Rock,Paper or Scissors?");
+    return ;
+    }   
+function playRound(getComputerChoice,getHumanChoice){
+
+    }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
+
+console.log(getComputerChoice());
+
