f1d8b2b12cab10e3c728df1faf5bba80f175c6a7
Reyuken <212830200+Reyuken@users.noreply.github.com>
2025-07-08 18:32:49 +0800

    Added buttons and rewrote js code 1st attempt Curent progress: - added rock paper scissors buttons to html. - when rock button is pressed, output of choice is shown in html and result of game is shown in console

--- javascript.js
+++ javascript.js
@@ -1,102 +1,93 @@
+document.addEventListener("DOMContentLoaded",()=>{
 let humanScore=0;
-let computerScore=0;
-function playGame(){
-    function scoring(){
-        console.log("Human:"+humanScore);
-        console.log("Computer:"+computerScore);
-
-        return 
-    }
-        //computer chooses a random number
-    function getComputerChoice(){
-        randomNumber = Math.floor(Math.random()*99);
-        if (randomNumber<=33){
-            return "rock";
-
-            }
-        else if(randomNumber>66){
-            return "paper";
-        }
-        else{
-            return "scissors";
-        }
-        
-        }
-        //prompts user to input choice
-    function getHumanChoice(userChoice){
-        userChoice=prompt("Rock,Paper or Scissors?").toLowerCase();
-        return userChoice;
-        }   
-
-
-
-        function playRound(computerChoice,userChoice){
-            //computer chooses rock
-            if (computerChoice=="rock" && userChoice=="rock"){
-                console.log("It's a tie!");
-                return ;
-            }
-            else if (computerChoice=="rock" && userChoice=="paper"){
-                humanScore++
-                console.log("You win!,Paper beats Rock.");
-                return ;
-            }
-            else if (computerChoice=="rock" && userChoice=="scissors"){
-                computerScore++
-                console.log("You lose!,Rock beats Scissors.");
-                return ;
-            }
-            //computer chooses paper
-                if (computerChoice=="paper" && userChoice=="rock"){
-                computerScore++    
-                console.log("You lose!, Paper beats Rock.");
-                return ;
-            }
-            else if (computerChoice=="paper" && userChoice=="paper"){
-                console.log("It's a tie!");
-                return ;
-            }
-            else if (computerChoice=="paper" && userChoice=="scissors"){
-                humanScore++
-                console.log("You win!,Scissors beats Paper.");
-                return ;
-            }
-            //computer chooses scissors
-                if (computerChoice=="scissors" && userChoice=="rock"){
-                humanScore++
-                console.log("You win!,Rock beats Scissors.");
-                return ;
-            }
-            else if (computerChoice=="scissors" && userChoice=="paper"){
-                computerScore++
-                console.log("You lose!,Scissor beats Paper.");
-                return ;
-            }
-            else if (computerChoice=="scissors" && userChoice=="scissors"){      
-                console.log("It's a tie!");
-                return ;
-            }
-            else {
-                games--
-                return console.log("Input a valid Choice!")
-
-            }
-            }
-            
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(computerSelection,humanSelection);
-scoring()
-}
+let computerScore=0;   
     
+    // function playGame()
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 99);
+    if (randomNumber <= 33) {
+        return "rock";
 
-for (games=0;games<5;games++){
-    playGame()
-}
+    }
+    else if (randomNumber > 66) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 
-if (humanScore>computerScore){
-    console.log("YOU WIN!!")
 }
-else{
-    console.log("YOU LOSE!DUMBASS HAHAHAHAH")
+function getHumanChoice(choice) {
+    document.getElementById("userChoice").innerHTML += "You chose: " + choice + "<br>";
+    return choice;
 }
+function playRound(computerChoice, userChoice) {
+    //computer chooses rock
+    if (computerChoice == "rock" && userChoice == "rock") {
+        console.log("It's a tie!");
+        return;
+    }
+    else if (computerChoice == "rock" && userChoice == "paper") {
+        humanScore++
+        console.log("You win!,Paper beats Rock.");
+        return;
+    }
+    else if (computerChoice == "rock" && userChoice == "scissors") {
+        computerScore++
+        console.log("You lose!,Rock beats Scissors.");
+        return;
+    }
+    //computer chooses paper
+    if (computerChoice == "paper" && userChoice == "rock") {
+        computerScore++
+        console.log("You lose!, Paper beats Rock.");
+        return;
+    }
+    else if (computerChoice == "paper" && userChoice == "paper") {
+        console.log("It's a tie!");
+        return;
+    }
+    else if (computerChoice == "paper" && userChoice == "scissors") {
+        humanScore++
+        console.log("You win!,Scissors beats Paper.");
+        return;
+    }
+    //computer chooses scissors
+    if (computerChoice == "scissors" && userChoice == "rock") {
+        humanScore++
+        console.log("You win!,Rock beats Scissors.");
+        return;
+    }
+    else if (computerChoice == "scissors" && userChoice == "paper") {
+        computerScore++
+        console.log("You lose!,Scissor beats Paper.");
+        return;
+    }
+    else if (computerChoice == "scissors" && userChoice == "scissors") {
+        console.log("It's a tie!");
+        return;
+    }
+    // else {
+    //     games--
+    //     return console.log("Input a valid Choice!")
+
+    // }
+}
+// Attach handlers
+document.getElementById("rockBtn").addEventListener("click", () => {
+    const humanSelection=getHumanChoice('rock');
+    const computerSelection=getComputerChoice();
+    playRound(computerSelection,humanSelection);
+});
+document.getElementById("paperBtn").addEventListener("click", () => getHumanChoice('paper'));
+document.getElementById("scissorsBtn").addEventListener("click", () => getHumanChoice('scissors'));
+
+// function players(){
+// const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// playRound(computerSelection,humanSelection);
+// }
+
+
+
+})
